5aa42b47a2010839588e363d83fe532b5623a34f
Nada Aboubakr <nadabakr8102@gmail.com>
2024-08-06 23:01:02 +0300

    >>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Add your Next.js configuration options here
+  };
+  
+  export default nextConfig;
+  
