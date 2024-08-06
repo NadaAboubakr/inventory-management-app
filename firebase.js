43c19033583fc7e67bb0f0471d683ececa14af0b
Nada Aboubakr <nadabakr8102@gmail.com>
2024-08-06 23:13:07 +0300

    hopefully fixed

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAnalytics} from "firebase/analytics";
+import { isSupported } from 'firebase/analytics';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,14 +23,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
-if (typeof window !== 'undefined') {
-  isSupported().then((supported) => {
-    if (supported) {
-      getAnalytics(app);
-    }
-  }).catch((error) => {
-    console.error('Error checking analytics support', error);
-  });
-}
+// Check if analytics is supported
+isSupported().then((supported) => {
+  if (supported) {
+    // Initialize analytics if supported
+    const analytics = getAnalytics(app);
+    console.log('Analytics initialized:', analytics);
+  } else {
+    console.log('Analytics is not supported in this environment.');
+  }
+}).catch((error) => {
+  console.error('Error checking if analytics is supported:', error);
+});
 
 export { firestore, app };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Add your Next.js configuration options here
   };
   
   export default nextConfig;
