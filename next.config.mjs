9e8aa267a414ed8285a1190d0b890aa291bd18d5
Nada Aboubakr <nadabakr8102@gmail.com>
2024-08-06 22:55:23 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,4 @@
-// @type {import('next').NextConfig} 
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// export default nextConfig;
-module.exports = {
-    experimental: {
-      // Add other experimental features if you use any
-    },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        require('./scripts/generate-sitemap');
-      }
-  
-      config.module.rules.push({
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'],
-          plugins: [['@babel/plugin-transform-runtime', { regenerator: true }]]
-        }
-      });
-  
-      return config;
-    },
-    reactStrictMode: true,
-  };
-  
+export default nextConfig;
